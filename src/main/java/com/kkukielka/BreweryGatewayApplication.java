0dcce9f7c824e1bbd09e1af891f2932fbfa9052f
Kamil <qkiz288@gmail.com>
2021-03-03 09:48:41 +0100

    Project setup

---
+++ src/main/java/com/kkukielka/BreweryGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.kkukielka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryGatewayApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kkukielka/BreweryGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kkukielka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
