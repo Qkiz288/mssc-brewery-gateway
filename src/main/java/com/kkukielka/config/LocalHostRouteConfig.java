e3af17b0447e611c191efac821b30c2045e96a90
Kamil <qkiz288@gmail.com>
2021-03-04 10:13:18 +0100

    RouteLocator config for localhost

---
+++ src/main/java/com/kkukielka/config/LocalHostRouteConfig.java
@@ -0,0 +1,24 @@
+package com.kkukielka.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostRouteConfig {
+
+    // ? matches one character
+    // * matches zero or more characters
+    // ** matches zero or more directories in a path
+    // more about AntPathMatcher:
+    // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/util/AntPathMatcher.html
+    @Bean
+    public RouteLocator localhostRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "api/v1/beerUpc/*")
+                    .uri("http://localhost:8080")
+                    .id("beer-service"))
+                .build();
+    }
+}
